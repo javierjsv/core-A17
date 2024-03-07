5d7bdb273cca86349403c5e92a9f730301e650a6
johansolis <johansolis@periferia-it.com>
2024-03-07 08:41:31 -0500

    structura de carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MaterialModule} from "./modules/material/material.module";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet , MaterialModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'core';
+
 }
