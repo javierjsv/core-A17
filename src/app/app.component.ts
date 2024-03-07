9d42fdde5a1e3652d86c4acdc54bcde69624fb97
johansolis <johansolis@periferia-it.com>
2024-03-07 08:57:44 -0500

    configuracion traductor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MaterialModule} from "./modules/material/material.module";
+// import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , MaterialModule ],
+  imports: [CommonModule, RouterOutlet, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
+/*  constructor(public translateService: TranslateService) {
+    this.initApp()
+  }*/
+
+  initApp():void{
+    /*const lang: any = localStorage.getItem('lang') ? localStorage.getItem('lang') : 'es';
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);*/
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+// importaciones propieas
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync()]
 };
