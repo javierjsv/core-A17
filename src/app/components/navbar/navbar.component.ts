2c75a21d860932851a5c11184df096267f26ffad
johansolis <johansolis@periferia-it.com>
2024-03-15 10:49:20 -0500

    feat : navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
 import {HelperService} from "../../services/helper.service";
+import {GlobalText} from "../../core/config/globlasTexts";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +12,12 @@ import {HelperService} from "../../services/helper.service";
 })
 export class NavbarComponent {
   home: string = 'home';
-  lang = 'es'
+  lang: string = 'es'
+  globalText = GlobalText;
   constructor(public router: Router ,
               private translateService: TranslateService ,
-              public helperService :HelperService) {
+              public helperService :HelperService,
+              public _snackBar: MatSnackBar) {
   }
   go():void{
     this.router.navigateByUrl('post')
@@ -23,5 +27,8 @@ export class NavbarComponent {
     this.lang === 'es' ? this.lang = "en" : this.lang = "es";
     this.lang === 'es' ? this.translateService.use('es') : this.translateService.use('en');
     this.helperService.setLocalSorage('lang' , this.lang )
+    this._snackBar.open(this.lang === 'es' ?  this.translateService.instant('SPANISH') :  this.translateService.instant('ENGLISH'), "OK" ,{
+      duration: 3000, horizontalPosition:"right"
+    });
   }
 }
--- src/app/core/config/globlasTexts.ts
+++ src/app/core/config/globlasTexts.ts
@@ -1,3 +1,6 @@
+const ASSESTS = 'assets'
+const IMAGES = 'img'
 export const GlobalText = {
-
+   IMG_ES:`${ASSESTS}/${IMAGES}/es.png`,
+   IMG_EN:`${ASSESTS}/${IMAGES}/en.png`,
 }
